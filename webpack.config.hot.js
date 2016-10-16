9007d7d2e0cc659d14e5e640f1214a71e299954f
Henry Paulino <henrypl360@gmail.com>
2016-10-16 13:22:35 -0400

    add webpack hot config and babelrc

---
+++ webpack.config.hot.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    `webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr`,
+    './src/index.js',
+    // './dist/styles.css'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:3000/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+        include: path.join(__dirname, 'src')
+      },
+      { test: /\.json$/, loader: 'json' },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+    ],
+    query: {
+      presets: ['react', 'es2015', 'stage-0']
+    }
+  }
+};
