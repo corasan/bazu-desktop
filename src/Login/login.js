23e1cb44bcdcfd7a5e05f798915d69aeb2ad24e0
Henry Paulino <henrypl360@gmail.com>
2016-10-18 10:14:10 -0400

    firebase config

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  login = () => {
+    fb.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+    .then((user) => console.log(user))
+    .catch((err) => console.log(err.message));
+  }
+
   render() {
     return (
       <div>
@@ -8,8 +22,8 @@ export default class Login extends Component {
 
         <div>
           <form>
-            <input type="email" placeholder="Email"/>
-            <input type="password" placeholder="Password"/>
+            <input type="email" placeholder="Email" onChange={ (email) => this.setState({email})}/>
+            <input type="password" placeholder="Password" onChange={ (password) => this.setState({password})}/>
           </form>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React, { Component } from 'react';
 import { Router, browserHistory } from 'react-router';
 import ReactDOM from 'react-dom';
 import routes from './routes';
+import fb from '../firebase.config.js';
 
 ReactDOM.render(<Router history={browserHistory}>{routes}</Router>, document.getElementById('app'));
